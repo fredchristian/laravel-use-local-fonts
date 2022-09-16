173d28ac62c39ece016ad2dfb31c143482108185
Fred <fred.christian@gmail.com>
2022-09-16 14:39:08 +0400

    Update authors.blade.php
    Correction typo

--- resources/views/components/ui/element/heading/authors.blade.php
+++ resources/views/components/ui/element/heading/authors.blade.php
@@ -4,6 +4,6 @@
 
 @if($names)
     <span class="block font-medium text-sm text-center md:text-left text-gray-400">
-        {{ __('Designed by :authors', ['authors' =>  Arr::join($names, ',', __(' and '))]) }}
+        {{ __('Designed by :authors', ['authors' =>  Arr::join($names, ', ', __(' and '))]) }}
     </span>
 @endif
